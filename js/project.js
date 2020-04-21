01316a6d02142427d6b0ab223e42dba6245416c2
Bobby Lim <bobbylim4@gmail.com>
2020-04-21 16:56:00 -0700

    View try

--- js/project.js
+++ js/project.js
@@ -3,19 +3,19 @@ var getRandomQuote;
 
 let quotes = [
 	{quote: "A great man is alway willing to be little.",
-	source: "-R.W. Emerson",
+	source: "-R.W. Emerson"
 	},
 	{quote: "The true soldier fights not because he hate what is in front of him, but because he loves what is behind him.", 
-	source: "-G.K. Chesterton",
+	source: "-G.K. Chesterton"
 	},
 	{quote: "When one tugs at a single thing in nature, he finds it attached to the rest of the world.",
-	source: "-John Muir",
+	source: "-John Muir"
 	},
 	{quote: "We don't see things as they are, we see them as we are.",
-	source: "-Anais Nin",
+	source: "-Anais Nin"
 	},
 	{quote: "They lived and laughed and loved and left.",
-	source: "-James Joyce",
+	source: "-James Joyce"
 ]
 
 document.write(quotes());
