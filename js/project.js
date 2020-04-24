ae29420e98aec6861d4df73844af01281dd219e4
Bobby Lim <bobbylim4@gmail.com>
2020-04-24 11:56:35 -0700

    Notes added

--- js/project.js
+++ js/project.js
@@ -1,12 +1,12 @@
 /**
 * Welcome to my first Team Treehouse Techdegree Project!
-* Full Stack Web Developer Project 1: A Random Quote Generator
+* Full Stack Web Developer Project 1: A Random Quote Generator.
 */
 
 /** 
-* The following array named 'quotes' includes 5 objects
-* Each object includes the properties 'quote' and 'source'
-* One of the object includes the additional properties 'citation' and 'year'
+* The following array named 'quotes' includes 5 objects of quotes.
+* Each object includes the properties 'quote' and 'source'.
+* One of the object includes the additional properties 'citation' and 'year'.
 */ 
 let quotes = [
 	{quote: "A great man is alway willing to be little.",
@@ -28,16 +28,16 @@ let quotes = [
 	},
 ];
 
-//The following function named 'getRandomQuote' randomizes the quote objects above and returns one of them when the function gets called
+//The following function named 'getRandomQuote' randomizes the quote objects above and returns one of them when the function gets called.
 function getRandomQuote() {
 	let randomQuote = Math.floor(Math.random() * quotes.length);
 	return quotes[randomQuote];
 };
 
 /**
-* The following function named 'printQuote' uses the 'getRandomQuote' function to write the returned quote to the webpage
-* The HTML string was added onto using template literals. The quote and source was added for every quote object 
-* The HTML string was then added onto using template literals if the quote object contained a citation or year using an 'if' statement
+* The following function named 'printQuote' uses the 'getRandomQuote' function to write the random returned quote to the webpage.
+* The HTML string was added onto using template literals. The quote and source was added for every quote object.
+* The HTML string was then added onto using template literals if the quote object contained a citation or year using an 'if' statement.
 */
 function printQuote() {
 	let myRandomQuote = getRandomQuote();
@@ -52,11 +52,11 @@ function printQuote() {
 	document.getElementById('quote-box').innerHTML = storeHTML;
 };
 
-//The 'printQuote' is called to return the final HTML string 
+//The 'printQuote' is called to return the final HTML string.
 printQuote();
 
 /**
-* The following code below was provided by Team Treehouse
-* It allows the webpage to show another random quote object from the array when the 'Show another quote' button is clicked on in the webpage
+* The following code below was provided by Team Treehouse.
+* It allows the webpage to show another random quote object from the 'quotes' array when the 'Show another quote' button is clicked on in the webpage.
 */
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
